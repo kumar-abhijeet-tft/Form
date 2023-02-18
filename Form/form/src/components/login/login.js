891189d214bcdce90f82cd07538081fae864f44e
RUPESH KUMAR <98460861+Rupeshkumar4@users.noreply.github.com>
2023-02-18 12:42:31 +0530

    Add files via upload

---
+++ Form/form/src/components/login/login.js
@@ -0,0 +1,33 @@
+import React, {useState} from "react"
+import "./login.css"
+
+const Login = () => {
+
+    const [user, setUser]  = useState({
+        email:"",
+        password:""
+    })
+
+    const handleChange = e => {
+        const {name, value } = e.target
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
+
+
+    return (
+        <div className="login">
+        {console.log("UserLogin",user)}
+            <h1>Login</h1>
+            <input type="text"  name="email" value={user.email} placeholder="Enter your Email" onChange={handleChange} ></input>
+            <input type="password" name="password" value={user.password} placeholder="Enter your Password" onChange={handleChange} ></input>
+            <div className="button">Login</div>
+            <div>or</div>
+            <div className="button">Register</div>
+        </div>
+    )
+}
+
+export default Login
