4a98a5cc2ce31f5c6af2e7dcb8b8d608f27d9971
Abhijeet Kumar <kumar.abhijeet@tftus.com>
2023-02-18 19:28:21 +0530

    changes

--- Form/form/src/components/login/login.js
+++ Form/form/src/components/login/login.js
@@ -1,29 +1,40 @@
-import React, {useState} from "react"
+import React, { useState, useRef } from "react"
 import "./login.css"
+import { Link, useHistory } from 'react-router-dom';
+
 
 const Login = () => {
+    const linkRef = useRef(null);
+    const history = useHistory()
 
-    const [user, setUser]  = useState({
-        email:"",
-        password:""
+    const [user, setUser] = useState({
+        email: "",
+        password: ""
     })
 
     const handleChange = e => {
-        const {name, value } = e.target
+        const { name, value } = e.target
         setUser({
             ...user,
             [name]: value
         })
     }
 
+    const handleLogin = () => {
+        // console.log("clicked");
+        // <Link to="../homepage/homepage"></Link>
+        history.push("/homepage");
+    };
 
     return (
         <div className="login">
-        {console.log("UserLogin",user)}
+            {console.log("UserLogin", user)}
             <h1>Login</h1>
-            <input type="text"  name="email" value={user.email} placeholder="Enter your Email" onChange={handleChange} ></input>
+            <input type="text" name="email" value={user.email} placeholder="Enter your Email" onChange={handleChange} ></input>
             <input type="password" name="password" value={user.password} placeholder="Enter your Password" onChange={handleChange} ></input>
-            <div className="button">Login</div>
+            <div className="button" onClick={handleLogin}>
+                Login
+            </div>
             <div>or</div>
             <div className="button">Register</div>
         </div>
